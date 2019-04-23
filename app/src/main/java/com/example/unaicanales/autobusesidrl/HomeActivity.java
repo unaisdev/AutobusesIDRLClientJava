2103f3d851287f6624b304ec7c83f99db03d57cf
Seretoiden <36845145+Seretoiden@users.noreply.github.com>
2019-04-23 09:28:49 +0200

    Retocamos el cliente para recibir alertas

--- app/src/main/java/com/example/unaicanales/autobusesidrl/HomeActivity.java
+++ app/src/main/java/com/example/unaicanales/autobusesidrl/HomeActivity.java
@@ -3,21 +3,30 @@
 import android.Manifest;
 import android.accounts.Account;
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.location.Location;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -82,9 +91,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Random;
 
-public class HomeActivity extends AppCompatActivity {
+        public class HomeActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID ="Alertas";
+    private NotificationManager notificationManager;
     private static final String TAG = "HomeActivity: ";
     private FusedLocationProviderClient mFusedLocationClient;
     private SettingsClient mSettingsClient;
@@ -180,6 +192,36 @@ public class HomeActivity extends AppCompatActivity {
                             paradas.add(nuevaParada);
 
                             break;
+
+                    case "alerta":
+                        //Recibimos el punto donde se encuentra el bus ahora, debido a que se encontraría en movimientoç
+                        Log.e("---> ", msg);
+
+                        String[] alerta = findData(msg).split(",");
+
+                        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                        //Setting up Notification channels for android O and above
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                            setupChannels();
+                        }
+                        int notificationId = new Random().nextInt(60000);
+
+                        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                .setSmallIcon(R.drawable.ic_bus)
+                                .setContentTitle(alerta[0])
+                                .setContentText(alerta[1]);
+
+
+                        NotificationManager notificationManager =
+                                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                        notificationManager.notify(notificationId /* ID of notification */, mBuilder.build());
+                        // Also if you intend on generating your own notifications as a result of a received FCM
+                        // message, here is where that should be initiated. See sendNotification method below.
+
+                        break;
+
                     }
                 }
             } catch (IOException e) {
@@ -189,6 +231,22 @@ public class HomeActivity extends AppCompatActivity {
             }
         }
     };
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(){
+        CharSequence channelName ="channelName";
+        String channelDesc = "channelDesc";
+
+        NotificationChannel notificationChannel;
+        notificationChannel = new NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_LOW);
+        notificationChannel.setDescription(channelDesc);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.RED);
+        notificationChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -285,7 +343,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public static JSONObject findLinea(String msg) throws JSONException {
-        return new JSONObject(msg.substring(msg.indexOf(':' + 1 , msg.length())));
+        return new JSONObject(msg.substring(msg.indexOf(':' + 1), msg.length()));
     }
 
     //Metodo que recoge el mensaje en sí
